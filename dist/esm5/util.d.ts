501b489c2c06898490f794e90a5472dc66cbd31b
Petr Chalov <tchalov.pyotr@yandex.ru>
2019-11-01 17:57:27 +0400

    remove dist

--- dist/esm5/util.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const uuid: () => string;
-export declare const transformResponse: (response: any) => {};
-export declare const transformRequest: (request: any) => {
-    callbacks: {};
-    payload: any[];
-};
--- dist/esm5/util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.uuid = function () {
-    var mask = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
-    // tslint:disable-next-line:no-bitwise
-    return mask.replace(/[xy]/g, function (c, r) { return ("x" === c ? (r = Math.random() * 16 | 0) : (r & 0x3 | 0x8)).toString(16); });
-};
-//# sourceMappingURL=util.js.map
